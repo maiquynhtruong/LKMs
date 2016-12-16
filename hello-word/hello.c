bc8979533f32fb93c9a9c00f7d37b6636656cf2a
Mai Truong <maitruong2801@gmail.com>
2016-12-17 01:56:50 +0700

    Update hello.c

--- hello-word/hello.c
+++ hello-word/hello.c
@@ -1,1 +1,22 @@
+#include <linux/module.h> // Needed for all modules
+#include <linux/kernel.h> // Needed for KERN_INFO
+#include <linux/init.h> // Needed for macros
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mai Truong");
+MODULE_DESCRIPTION("A Hello World Loadable Kernel Module (LKM)");
+MODULE_VERSION("0.1");
+
+static int __init hello_start(void) {
+        printk(KERN_INFO "Loading hello module...\n");
+        printk(KERN_INFO "Hello World...\n");
+        return 0;
+}
+
+static void __exit hello_end(void) {
+        printk(KERN_INFO "Bye bye\n");
+}
+
+module_init(hello_start);
+module_exit(hello_end);
+~                       
